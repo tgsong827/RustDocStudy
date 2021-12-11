118bb6e30fc5eaf1e2950afb0586341a137e7782
tgsong <tgsong827@gmail.com>
2021-12-11 23:10:22 +0900

    12.6 표준 출력 대신 표준에러로 변경

--- greprs/src/main.rs
+++ greprs/src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err|{
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
@@ -16,7 +16,7 @@ fn main() {
     // println!("In File {}", config.filename);
 
     if let Err(e) = greprs::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
 
         process::exit(1);
     }
