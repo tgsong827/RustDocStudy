bf9db9cde44b7f6d8bb7f2850df53cdcd2066020
Taegi Song <tgsong827@gmail.com>
2021-12-26 18:14:52 +0900

    13.2 반복자 기본 완료

--- closure/src/main.rs
+++ closure/src/main.rs
@@ -1,13 +1,49 @@
 use std::{thread, time::{Duration}};
 
-fn main() {
-    let simulated_user_specified_value = 10;
-    let simulated_random_number = 7;
+struct Cacher<T> where T: Fn(u32) -> u32 {
+    calculation: T,
+    value: Option<u32>,
+}
 
-    generate_workout (
-        simulated_user_specified_value,
-        simulated_random_number
-    );
+impl<T> Cacher<T> where T: Fn(u32) -> u32 {
+    fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation,
+            value: None,
+        }
+    }
+
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            },
+        }
+    }
+}
+
+fn main() {
+    // let simulated_user_specified_value = 10;
+    // let simulated_random_number = 7;
+
+    // generate_workout (
+    //     simulated_user_specified_value,
+    //     simulated_random_number
+    // );
+
+    // closure 환경 capture
+    let x = vec![1, 2, 3];
+
+    let equal_to_x = move |z| z == x;
+
+    // println!("cant't use x here: {:?}", x);
+
+    let y = vec![1, 2, 3];
+
+    assert!(equal_to_x(y));
 
 }
 
@@ -19,20 +55,26 @@ fn main() {
 // }
 
 fn generate_workout(intensity: u32, random_number: u32) {
-    let expensive_closure = |num: u32| -> u32 {
+    let mut expensive_result = Cacher::new(|num| {
         println!("calculating slowly...");
         thread::sleep(Duration::from_secs(2));
         num
-    };
+    });
+    
+    // let expensive_closure = |num: u32| -> u32 {
+    //     println!("calculating slowly...");
+    //     thread::sleep(Duration::from_secs(2));
+    //     num
+    // };
 
     if intensity < 25 {
         println!(
             "Today, do {} pushups!",
-            expensive_closure(intensity)
+            expensive_result.value(intensity)
         );
         println!(
             "Next, do {} situps!",
-            expensive_closure(intensity)
+            expensive_result.value(intensity)
         );
     } else {
         if random_number == 3 {
@@ -40,7 +82,7 @@ fn generate_workout(intensity: u32, random_number: u32) {
         } else {
             println!(
                 "Today, run for {} minutes!",
-                expensive_closure(intensity)
+                expensive_result.value(intensity)
             );
         }
     }
