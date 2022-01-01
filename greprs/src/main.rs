f2a45382e1867f51ae5cfd013c7127c42bcf64cd
Taegi Song <tgsong827@gmail.com>
2022-01-01 18:20:57 +0900

    13장 함수형 언어의 등성들, 반복자와 클로저 완료

--- greprs/src/main.rs
+++ greprs/src/main.rs
@@ -5,9 +5,9 @@ use std::{env, process};
 use greprs::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    // let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args).unwrap_or_else(|err|{
+    let config = Config::new(env::args()).unwrap_or_else(|err|{
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
