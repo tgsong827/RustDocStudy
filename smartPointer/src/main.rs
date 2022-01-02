903a02e0a6473d9762acb0161c80cceca86eb967
Taegi Song <tgsong827@gmail.com>
2022-01-02 17:25:32 +0900

    finished 15.5 RefCell

--- smartPointer/src/main.rs
+++ smartPointer/src/main.rs
@@ -1,9 +1,12 @@
 use List::{Cons, Nil};
+use std::cell::RefCell;
 use std::ops::Deref;
 use std::rc::Rc;
 
+#[derive(Debug)]
+
 enum List {
-    Cons(i32, Rc<List>),
+    Cons(Rc<RefCell<i32>>, Rc<List>),
     Nil,
 }
 
@@ -39,13 +42,19 @@ fn main() {
     // drop(c);
     // println!("CustomSmartPointers created.");
 
-    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
-    println!("count after creating a = {}", Rc::strong_count(&a));
-    let b = Cons(3, Rc::clone(&a));
-    println!("count after creating b = {}", Rc::strong_count(&a));
-    {
-        let c = Cons(4, Rc::clone(&a));
-        println!("count after creating c = {}", Rc::strong_count(&a));
-    }
-    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
+    let value = Rc::new(RefCell::new(5));
+
+    let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
+
+    let b = Cons(Rc::new(RefCell::new(6)), Rc::clone(&a));
+    let c = Cons(Rc::new(RefCell::new(10)), Rc::clone(&a));
+
+    *value.borrow_mut() += 10;
+
+    println!("a after = {:?}", a);
+    println!("b after = {:?}", b);
+    println!("c after = {:?}", c);
+
+    // let x = 5;
+    // let y = &mut x;
 }
