2fe7c4bc6426e56a05f749ba8372dba601159266
tgsong <tgsong827@gmail.com>
2022-01-02 21:39:48 +0900

    working 16.3 공유상태

---
+++ concurrency/src/main.rs
@@ -0,0 +1,49 @@
+use std::thread;
+use std::time::Duration;
+use std::sync::mpsc;
+
+fn main() {
+
+    // let v = vec![1, 2, 3];
+
+    // let handle = thread::spawn(move || {
+    //     println!("Here's a vector: {:?}", v);
+    // });
+
+    // handle.join().unwrap();
+
+    let (tx, rx) = mpsc::channel();
+
+    let tx1 = mpsc::Sender::clone(&tx);
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+        
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+        ];
+        
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {}", received);
+    }
+}
