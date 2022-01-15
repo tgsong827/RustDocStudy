fe3bcfbfbd8d908d68be6e6704dfcee2329c05fa
Taegi Song <tgsong827@gmail.com>
2022-01-15 19:39:27 +0900

    working 19.2 고급 라이프타임

---
+++ advanced/src/lib.rs
@@ -0,0 +1,15 @@
+struct Context<'a>(&'a str);
+
+struct Parser<'a> {
+    context: &'a Context<'a>,
+}
+
+impl<'a> Parser<'a> {
+    fn parse(&self) -> Result<(), &str> {
+        Err(&self.context.0[1..])
+    }
+}
+
+fn parse_context(context: Context) -> Result<(), &str> {
+    Parser { context: &context }.parse()
+}
